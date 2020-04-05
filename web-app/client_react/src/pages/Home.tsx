6230d7b8307142ebb90995f921bce0df282fa6c6
ko-he <localhost.ko@gmail.com>
2020-04-05 18:38:20 +0900

    create home page

---
+++ web-app/client_react/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type HomePageProps = {}
+
+const HomePage: React.FC<HomePageProps> = _props => {
+  return <p>HomePage</p>
+}
+
+export default HomePage
--- web-app/client_react/src/routers/AppRouter.tsx
+++ web-app/client_react/src/routers/AppRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from '../pages/Home'
 
 type AppRouterProps = {}
 const AppRouter: React.FC<AppRouterProps> = _props => {
   return(
     <Router>
       <Switch>
-        <Route exact path="/" component={React.Fragment} />
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </Router>
   )
