1bd8adfa43e783d8c7ee22268ba1ed5c0079d154
ko-he <localhost.ko@gmail.com>
2020-04-05 18:43:53 +0900

    rename page

--- web-app/client_react/src/routers/AppRouter.tsx
+++ web-app/client_react/src/routers/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HomePage from '../pages/Home'
+import HomePage from '../pages/HomePage'
 
 type AppRouterProps = {}
 const AppRouter: React.FC<AppRouterProps> = _props => {
